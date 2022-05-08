481fd2a66de4490c439697d7918d43c2fa8ba746
FernandoTDF <104107236+FernandoTDF@users.noreply.github.com>
2022-05-08 19:27:51 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+document.getElementById("rotulo1").innerHTML = "Ingrese la vuelta 1: ";
+rotulo2.innerHTML = "Ingrese la vuelta 2: ";
+rotulo3.innerHTML = "Ingrese la vuelta 3: ";
+rotulo4.innerHTML = "Ingrese la vuelta 4: ";
+let vuelta1: number = dato1.value;
+let vuelta2: number = dato2.value;
+let vuelta3: number = dato3.value;
+let vuelta4: number = dato4.value;
+let tiempoTotal = vuelta1 + vuelta2 + vuelta3 + vuelta4;
+console.log("Tiempo total; ", tiempoTotal);
+console.log("Promedio de vuelta: ", tiempoTotal / 4);
